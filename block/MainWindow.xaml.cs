5d4aa114b48c793a711318c9de310fe9b24b3044
Tomi09282 <112693387+Tomi09282@users.noreply.github.com>
2024-09-09 14:03:32 +0200

    Update MainWindow.xaml.cs

--- block/MainWindow.xaml.cs
+++ block/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace block
 {
@@ -16,9 +20,90 @@ namespace block
     /// </summary>
     public partial class MainWindow : Window
     {
+        static ObservableCollection<BSX> Blokkok = new ObservableCollection<BSX>();
         public MainWindow()
         {
             InitializeComponent();
         }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog()
+            {
+                Filter = "BSX Fájlok | *.bsx",
+                Multiselect = false
+            };
+
+            while (ofd.ShowDialog() == true)
+            {
+                if (ofd.FileName != "")
+                {
+                    XDocument xaml = XDocument.Load(ofd.FileName);
+                    foreach (var elem in xaml.Descendants("Item"))
+                    {
+                        String Id = elem.Element("ItemID").Value;
+                        String ItemName = elem.Element("ItemName").Value;
+                        String CategoryName = elem.Element("CategoryName").Value;
+                        String ColorName = elem.Element("ColorName").Value;
+                        String Qty = elem.Element("Qty").Value;
+
+                        BSX bs = new BSX(Id, ItemName, CategoryName, ColorName, Convert.ToInt32(Qty));
+                        Blokkok.Add(bs);
+                            
+                    }
+                    dtgAdatok.ItemsSource = Blokkok;
+                    tbCategory.ItemsSource = Blokkok.Select(x => x.CategoryName).Distinct().ToList();
+                    break;
+                }
+                else
+                {
+                    MessageBox.Show("Nincs Kiválasztva File");
+                    break;
+                }
+            }
+        }
+
+        private void tbNev_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Filter();
+        }
+
+        private void tbAzon_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Filter();
+        }
+        private void tbCategory_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Filter();
+        }
+
+
+        public void Filter()
+        {
+            ObservableCollection<BSX> newBlocks = new ObservableCollection<BSX>();
+            //Blokkok.Clear();
+            //var newlist = Blokkok.Select(x => x).Where(x => x.ItemName.StartsWith(tbNev.Text));
+            foreach (var item in Blokkok)
+            {
+                if (item.ItemName.StartsWith(tbNev.Text))
+                {
+                    String Id = item.ItemID;
+                    String ItemName = item.ItemName;
+                    String CategoryName = item.CategoryName;
+                    String ColorName = item.ColorName;
+                    String Qty = item.Qty.ToString();
+
+                    BSX bs = new BSX(Id, ItemName, CategoryName, ColorName, Convert.ToInt32(Qty));
+                    newBlocks.Add(bs);
+                }
+                else if(tbNev.Text == "")
+                {
+                    dtgAdatok.ItemsSource = Blokkok;
+                }
+            }
+            dtgAdatok.ItemsSource = newBlocks;
+        }
+
     }
 }
